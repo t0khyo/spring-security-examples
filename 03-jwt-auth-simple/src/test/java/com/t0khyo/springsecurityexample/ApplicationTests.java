c83fb5cd24fa130d0a8841e2426e1dcda8bab836
t0khyo <abdullrhman.eltoukhy@gmail.com>
2024-09-26 22:40:19 +0300

    First commit 🚀

---
+++ 03-jwt-auth-simple/src/test/java/com/t0khyo/springsecurityexample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t0khyo.springsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
