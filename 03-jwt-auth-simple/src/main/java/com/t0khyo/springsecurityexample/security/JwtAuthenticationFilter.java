bd483dde97d1f3dea756a82e9dc77a2b2a819086
t0khyo <abdullrhman.eltoukhy@gmail.com>
2024-09-27 18:46:42 +0300

    feat: JwtAuthenticationFilter

---
+++ 03-jwt-auth-simple/src/main/java/com/t0khyo/springsecurityexample/security/JwtAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package com.t0khyo.springsecurityexample.security;
+
+import com.nimbusds.jwt.SignedJWT;
+import com.t0khyo.springsecurityexample.exception.InvalidAccessTokenException;
+import com.t0khyo.springsecurityexample.exception.InvalidSignatureException;
+import com.t0khyo.springsecurityexample.exception.TokenExpiredException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            logger.debug("No JWT token found in request headers or token format is invalid");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String jwt = authHeader.substring(7);
+        final String username;
+        final SignedJWT verifiedJwt;
+
+        try {
+            verifiedJwt = jwtUtil.validateToken(jwt);
+            username = jwtUtil.extractUsername(verifiedJwt);
+
+            if (username != null && !username.isBlank() && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (TokenExpiredException ex) {
+            logger.warn("Token expired", ex);
+            throw ex;
+
+        } catch (InvalidSignatureException ex) {
+            logger.warn("Invalid JWT signature", ex);
+            throw ex;
+
+        } catch (Exception ex) {
+            logger.error("Token validation failed", ex);
+            throw new InvalidAccessTokenException("JWT token validation failed");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
